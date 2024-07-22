92110618dfe592b8aa23b7155b376c02b12e8f11
meet sathavare <meetsathavara10@gmail.com>
2024-07-22 23:59:24 +0530

    com

--- node/server.js
+++ node/server.js
@@ -3,7 +3,7 @@ const app = express()
 // require('dotenv').config()
 
 
-const port = process.env.PORT; 
+const port = process.env.PORT || 3000; 
 
 // app.use(function(req, res, next) {
 //     // res.status(400);
